6a66d92cb9ea78bea1c2c347e61dcc7cb55383f3
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-07-21 05:14:35 +0000

    [Add]管理者ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   
   namespace :admin do
     root to: "homes#top" 
+    resources :products, only: [:index, :show, :new, :create, :edit, :update]
+    resources :genres, only: [:index, :show, :create, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+    resources :customers, only: [:index, :show]
   end
   #顧客用
   devise_for :customers, skip: [:passwords], controllers: {
