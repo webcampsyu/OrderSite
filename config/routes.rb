42ea78188fc4b90ea82088aa35e5a7ee8d9d3cf1
ShutoKonno <129284912+webcampsyu@users.noreply.github.com>
2023-07-22 01:44:52 +0000

    [Finish]顧客側ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,30 @@ Rails.application.routes.draw do
     resources :order_details, only: [:show]
     resources :customers, only: [:index]
   end
+  
   #顧客用
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  
+  scope module: :public do 
+    root to: "homes#top"
+    get 'customers/mypage' => 'customers#show', as: 'mypage'
+    get '/genre/:id' => 'homes#index', as: 'index'
+    resources :products, only: [:index, :show]
+    resources :site_names, only: [:index, :show, :create, :edit, :update]
+    resources :orders, only: [:new, :index, :show, :create] do
+      collection do
+        post 'confirm'
+        get 'thanks'
+      end
+    end
+    resources :product_items, only: [:index, :create, :update, :destroy] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
